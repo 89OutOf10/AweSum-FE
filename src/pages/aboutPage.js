4b3fe32a95c5b932ff28a3e2edd73de2fbb6ad34
thals1214 <thaxls@ewhain.net>
2022-02-19 21:17:45 +0900

    1st commit -edited

---
+++ src/pages/aboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aboutPage = () => {
+  return (
+    <>
+      <h1>About 페이지</h1>
+    </>
+  );
+};
+
+export default aboutPage;
---
+++ src/pages/mainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../components/common/Button';
+
+const mainPage = () => {
+  return (
+    <>
+      <h1>메인 페이지</h1>
+      <Button>ENTER</Button>
+    </>
+  );
+};
+
+export default mainPage;
