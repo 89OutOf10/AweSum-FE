2d47ef556bed25409124dec776d51e244b3740fe
thals1214 <thaxls@ewhain.net>
2022-02-23 15:34:37 +0900

    updated - button

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,19 +3,28 @@ import styled from 'styled-components';
 import palette from '../../styles/palette';
 
 const StyledButton = styled.button`
-  border: none;
   border-radius: 4px;
+  border-color: ${palette.purple[0]};
+  height: 3rem;
   font-size: 1rem;
-  font-weight: bold;
   padding: 0.25rem 1rem;
-  color: white;
+  color: ${palette.purple[0]};
   outline: none;
   cursor: pointer;
+  font-style: normal;
+  font-weight: 900;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
 
-  background: ${palette.purple[1]};
+  background: ${palette.white[0]};
 
   &:hover {
-    background: ${palette.purple[1]};
+    background: ${palette.purple[0]};
+    color: ${palette.white[0]};
   }
 `;
 
--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+import Button from '../components/common/Button';
 
 const aboutPage = () => {
   return (
     <>
       <h1>About 페이지</h1>
+      <Button>button</Button>
     </>
   );
 };
