b922f32b7d3eec66c22b27bae255ecc1bf049e2c
thals1214 <thaxls@ewhain.net>
2022-05-15 08:36:47 +0900

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 // 서버 라우터
-export const USER_SERVER =
-  'http://ec2-13-125-206-79.ap-northeast-2.compute.amazonaws.com/';
+export const USER_SERVER = 'https://awesum.site/';
 
 export const DEFAULT_VIDEO = 'https://www.youtube.com/watch?v=hmyjdCfeXUo';
