568ab786302f0fb377cf5fecf6851127a9201d0e
thals1214 <thaxls@ewhain.net>
2022-05-15 20:32:33 +0900

    fix minor

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // 서버 라우터
-export const USER_SERVER = 'https://awesum.site/';
+export const USER_SERVER = 'https://awesum.site';
 
 export const DEFAULT_VIDEO = 'https://www.youtube.com/watch?v=hmyjdCfeXUo';
